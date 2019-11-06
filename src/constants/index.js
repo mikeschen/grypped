6fbf3936a80d41a4490cbe142c3e3e6566854af5
Michael Schennum <mikeschenphoto@gmail.com>
2019-11-06 14:17:12 -0700

    garbage cleanup

--- src/constants/index.js
+++
@@ -1,7 +0,0 @@
-const ROOT_URL = "https://www.mountainproject.com/data/get-ticks?key=106635196-7386eb17c9b59aaa02314e6588deb35d&";
-const ROUTES_URL = "https://www.mountainproject.com/data/get-routes?key=106635196-7386eb17c9b59aaa02314e6588deb35d";
-
-export default {
-  ROOT_URL: ROOT_URL,
-  ROUTES_URL: ROUTES_URL
-}
