2526a823289c824f778bcb84dccba8910c23dbbe
Michael Schennum <mikeschenphoto@gmail.com>
2019-11-13 15:42:27 -0700

    linting

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,215 +1,239 @@
 <template>
-    <div class="ticks">
+  <div class="ticks">
     <Search @searchName="getTicks" />
-      <div class="graph-box">
-        <b-container>
-          <b-row>
-            <b-col>
-              <div role="tooltip" ref="tooltip" :class="{'is-active': tooltipData}">
-                <div class="tooltip-container" v-if="tooltipData">
-                  <strong>Ticks: {{tooltipData.data[0]}}</strong>
-                </div>
+    <div class="graph-box">
+      <b-container>
+        <b-row>
+          <b-col>
+            <div role="tooltip" ref="tooltip" :class="{'is-active': tooltipData}">
+              <div class="tooltip-container" v-if="tooltipData">
+                <strong>Ticks: {{tooltipData.data[0]}}</strong>
               </div>
-              <svg style="width:0; height:0; position:absolute;" aria-hidden="true" focusable="false">
-                <defs>
-                  <linearGradient id="grpFill" x1="1" x2="1" y1="0" y2="1">
-                    <stop offset="0%" stop-color="#39af77"></stop>
-                    <stop offset="100%" stop-color="#ffffff"></stop>
-                  </linearGradient>
-                </defs>
-              </svg>
-              <TrendChart
-                  :datasets="quantitySets"
-                  :labels="quantityLabels"
-                  :min="0"
-                  :interactive=true
-                  @mouseMove="onMouseMove" class="tick-chart"
-                  :grid="{verticalLines: true, horizontalLines: true}"
-              />
+            </div>
+            <svg
+              style="width:0; height:0; position:absolute;"
+              aria-hidden="true"
+              focusable="false"
+            >
+              <defs>
+                <linearGradient id="grpFill" x1="1" x2="1" y1="0" y2="1">
+                  <stop offset="0%" stop-color="#39af77" />
+                  <stop offset="100%" stop-color="#ffffff" />
+                </linearGradient>
+              </defs>
+            </svg>
+            <TrendChart
+              :datasets="quantitySets"
+              :labels="quantityLabels"
+              :min="0"
+              :interactive="true"
+              @mouseMove="onMouseMove"
+              class="tick-chart"
+              :grid="{verticalLines: true, horizontalLines: true}"
+            />
 
-              <div class="mt-2"><strong>Hardest Send: V{{ Object.keys(sends).pop() }}</strong></div>
-            </b-col>
-          </b-row>
-        </b-container>
-      </div>
+            <div class="mt-2">
+              <strong>Hardest Send: V{{ Object.keys(sends).pop() }}</strong>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
     </div>
+  </div>
 </template>
 
 <script>
-  import axios from 'axios';
-  import { loadProgressBar } from 'axios-progress-bar'
-  import Search from "./Search.vue";
-  import TrendChart from "vue-trend-chart";
-  import Constants from '../constants';
-  import Popper from "popper.js";
+import axios from "axios";
+import { loadProgressBar } from "axios-progress-bar";
+import Search from "./Search.vue";
+import TrendChart from "vue-trend-chart";
+import Constants from "../constants";
+import Popper from "popper.js";
 
-  export default {
-    components: {
-      Search,
-      TrendChart
-    },
-    data() {
-      return {
-        quantityLabels: {
-          xLabels: ["VO", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12"],
-          yLabels: 4,
-          yLabelsTextFormatter: val => Math.round(val)
-        },
-        sends: {},
-        quantitySets:[],
-        tooltipData: null,
-        popper: null,
-        popperIsActive: false
+export default {
+  components: {
+    Search,
+    TrendChart
+  },
+  data() {
+    return {
+      quantityLabels: {
+        xLabels: [
+          "VO",
+          "V1",
+          "V2",
+          "V3",
+          "V4",
+          "V5",
+          "V6",
+          "V7",
+          "V8",
+          "V9",
+          "V10",
+          "V11",
+          "V12"
+        ],
+        yLabels: 4,
+        yLabelsTextFormatter: val => Math.round(val)
+      },
+      sends: {},
+      quantitySets: [],
+      tooltipData: null,
+      popper: null,
+      popperIsActive: false
+    };
+  },
+  methods: {
+    userId(value) {
+      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (re.test(String(value).toLowerCase())) {
+        return `email=${value}`;
+      } else {
+        return `userId=${value}`;
       }
     },
-    methods: {
-      userId(value) {
-        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if(re.test(String(value).toLowerCase())) {
-          return `email=${value}`
-        } else {
-          return `userId=${value}`
-        }
-      },
-      getTicks(value) {
-        loadProgressBar();
-        const routes = [];
-        const boulders = [];
-        const points = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        axios
-          .get(Constants.ROOT_URL + this.userId(value), {crossdomain: true})
-          .then((res) => {
-            res.data.ticks.forEach((tick) => {
-              routes.push(tick.routeId);
+    getTicks(value) {
+      loadProgressBar();
+      const routes = [];
+      const boulders = [];
+      const points = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      axios
+        .get(Constants.ROOT_URL + this.userId(value), { crossdomain: true })
+        .then(res => {
+          res.data.ticks.forEach(tick => {
+            routes.push(tick.routeId);
+          });
+          const routeList = routes.join();
+          const routesUrl = `${Constants.ROUTES_URL}&routeIds=${routeList}`;
+          axios.get(routesUrl, { crossdomain: true }).then(res => {
+            res.data.routes.forEach(route => {
+              if (route.type === "Boulder") {
+                boulders.push(parseInt(route.rating[1] + route.rating[2]));
+              }
             });
-            const routeList = routes.join();
-            const routesUrl = `${Constants.ROUTES_URL}&routeIds=${routeList}`;
-            axios.get(routesUrl, {crossdomain: true}).then((res) => {
-              res.data.routes.forEach((route) => {
-                if (route.type === "Boulder")
-                {
-                  boulders.push(parseInt(route.rating[1] + route.rating[2]))
-                }
-              });
-              this.sends = boulders.reduce((obj, item) => {
-                obj[item] = (obj[item] || 0) + 1;
-                return obj;
-              }, {});
-              Object.keys(this.sends).map((key) => {
-                points[key] = this.sends[key];
-              });
-              this.quantitySets = [{
+            this.sends = boulders.reduce((obj, item) => {
+              obj[item] = (obj[item] || 0) + 1;
+              return obj;
+            }, {});
+            Object.keys(this.sends).map(key => {
+              points[key] = this.sends[key];
+            });
+            this.quantitySets = [
+              {
                 data: points,
                 smooth: true,
                 fill: true,
                 showPoints: true,
-                className: 'curve-vue'
-              }]
-            })
-          })
-          .catch((err) => {
-            console.log("Error: Could Not Complete Request");
-            alert("Could Not Find User.", err);
-          });
-        },
-        initPopper() {
-          const chart = document.querySelector(".tick-chart");
-          const ref = chart.querySelector(".active-line");
-          const tooltip = this.$refs.tooltip;
-          this.popper = new Popper(ref, tooltip, {
-            placement: "right",
-            modifiers: {
-              offset: { offset: "0,10" },
-              preventOverflow: {
-                boundariesElement: chart
+                className: "curve-vue"
               }
-            }
+            ];
           });
-        },
-        onMouseMove(params) {
-          this.popperIsActive = !!params;
-          this.popper.scheduleUpdate();
-          this.tooltipData = params || null;
-        }
+        })
+        .catch(err => {
+          console.log("Error: Could Not Complete Request");
+          alert("Could Not Find User.", err);
+        });
     },
-    mounted() {
-      this.getTicks("mikemikaels@yahoo.com"),
-      this.initPopper()
+    initPopper() {
+      const chart = document.querySelector(".tick-chart");
+      const ref = chart.querySelector(".active-line");
+      const tooltip = this.$refs.tooltip;
+      this.popper = new Popper(ref, tooltip, {
+        placement: "right",
+        modifiers: {
+          offset: { offset: "0,10" },
+          preventOverflow: {
+            boundariesElement: chart
+          }
+        }
+      });
+    },
+    onMouseMove(params) {
+      this.popperIsActive = !!params;
+      this.popper.scheduleUpdate();
+      this.tooltipData = params || null;
     }
+  },
+  mounted() {
+    this.getTicks("mikemikaels@yahoo.com"), this.initPopper();
   }
+};
 </script>
 
 <style lang="scss">
-    body {
-        padding: 0;
-        margin: 0;
-        font-family: "Source Sans Pro", sans-serif;
-    }
+body {
+  padding: 0;
+  margin: 0;
+  font-family: "Source Sans Pro", sans-serif;
+}
 
-    #app {
-        padding: 20px 0;
-    }
+#app {
+  padding: 20px 0;
+}
 
-    ::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
-      color: #CACACA !important;
-      opacity: 1; /* Firefox */
-    }
+::placeholder {
+  /* Chrome, Firefox, Opera, Safari 10.1+ */
+  color: #cacaca !important;
+  opacity: 1; /* Firefox */
+}
 
-    :-ms-input-placeholder { /* Internet Explorer 10-11 */
-      color: #CACACA !important;
-    }
+:-ms-input-placeholder {
+  /* Internet Explorer 10-11 */
+  color: #cacaca !important;
+}
 
-    ::-ms-input-placeholder { /* Microsoft Edge */
-      color: #CACACA !important;
-    }
+::-ms-input-placeholder {
+  /* Microsoft Edge */
+  color: #cacaca !important;
+}
 
-    .ticks {
-      .vtc {
-          height: 250px;
-          font-size: 12px;
-      @media (min-width: 768px) {
-          height: 350px;
-      }
+.ticks {
+  .vtc {
+    height: 250px;
+    font-size: 12px;
+    @media (min-width: 768px) {
+      height: 350px;
     }
-    .grid,
-    .labels {
+  }
+  .grid,
+  .labels {
+    line {
+      stroke: rgba(#f69119, 0.5);
+    }
+  }
+  .x-labels {
+    .label {
       line {
-        stroke: rgba(#f69119, 0.5);
+        opacity: 0.3;
       }
-    }
-      .x-labels {
-        .label {
-        line {
-            opacity: 0.3;
-        }
       &:nth-child(6n + 1),
-        &:first-child {
-          text {
-              display: block;
-          }
-          line {
-              opacity: 1;
-          }
+      &:first-child {
+        text {
+          display: block;
+        }
+        line {
+          opacity: 1;
         }
       }
     }
   }
-  .curve-vue {
-    .stroke {
-      stroke: #39af77;
-    }
-    .fill {
-      fill: url(#grpFill);
-      fill-opacity: 0.5;
-    }
-    .point {
-      stroke-width: 2;
-      transition: stroke-width 0.2s;
-      fill: #39af77;
-      stroke: #39af77;
-    }
-    .point.is-active {
-      stroke-width: 5;
-    }
+}
+.curve-vue {
+  .stroke {
+    stroke: #39af77;
   }
+  .fill {
+    fill: url(#grpFill);
+    fill-opacity: 0.5;
+  }
+  .point {
+    stroke-width: 2;
+    transition: stroke-width 0.2s;
+    fill: #39af77;
+    stroke: #39af77;
+  }
+  .point.is-active {
+    stroke-width: 5;
+  }
+}
 </style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,60 +1,67 @@
 <template>
-    <div>
-      <b-container>
-        <b-row>
-          <b-col class="col-logo">
-            <img class="logo" alt="logo" src="../assets/climber.png">
-          </b-col>
-          <b-col cols="8">
-            <b-input-group prepend="Mountain Project Email or User ID" class="mt-3">
-              <b-form-input type="email" id="userInputEmail" aria-describedby="emailHelp" placeholder="name@email.com" name="userName" v-model="text" @keydown.enter="searchName(text)"></b-form-input>
-              <b-input-group-append>
-                <b-button variant="outline-success" @click="searchName(text)">Submit</b-button>
-              </b-input-group-append>
-            </b-input-group>
-          </b-col>
-        </b-row>
-      </b-container>
-    </div>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col class="col-logo">
+          <img class="logo" alt="logo" src="../assets/climber.png" />
+        </b-col>
+        <b-col cols="8">
+          <b-input-group prepend="Mountain Project Email or User ID" class="mt-3">
+            <b-form-input
+              type="email"
+              id="userInputEmail"
+              aria-describedby="emailHelp"
+              placeholder="name@email.com"
+              name="userName"
+              v-model="text"
+              @keydown.enter="searchName(text)"
+            ></b-form-input>
+            <b-input-group-append>
+              <b-button variant="outline-success" @click="searchName(text)">Submit</b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
-  export default {
-    props: {
-      method: { type: Function },
-    },
-    data() {
-      return {
-        text: ''
-      }
-    },
-    methods: {
-      searchName(text) {
-        this.$emit('searchName', text)
-      }
+export default {
+  props: {
+    method: { type: Function }
+  },
+  data() {
+    return {
+      text: ""
+    };
+  },
+  methods: {
+    searchName(text) {
+      this.$emit("searchName", text);
     }
   }
+};
 </script>
 
 <style lang="scss" scoped>
-  .logo {
-    width: 120px;
-  }
-  .input-email {
-    float: left;
-    display: inline-block;
-    width: 80%;
-  }
-  .btn-enter {
-    float: left;
-  }
+.logo {
+  width: 120px;
+}
+.input-email {
+  display: inline-block;
+  width: 80%;
+}
+.btn-enter {
+  float: left;
+}
 
-  @media (max-width: 768px) {
-    .col-logo {
-      display: none;
-    }
-    .input-group-append {
-      width: 100%;
-    }
+@media (max-width: 768px) {
+  .col-logo {
+    display: none;
   }
-  </style>
+  .input-group-append {
+    width: 100%;
+  }
+}
+</style>
